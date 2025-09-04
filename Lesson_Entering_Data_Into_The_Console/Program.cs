9096c07847e9f5d1cb9e3599ddd07aa8bfd11da3
Nikita  Vakarenko <138662704+usernamedudh@users.noreply.github.com>
2025-09-04 22:58:21 +0300

    Add project files.

---
+++ Lesson_Entering_Data_Into_The_Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_Entering_Data_Into_The_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entering_Data_Into_The_Console
+            Console.Write("Enter your data:");
+            string data = Console.ReadLine();
+            Console.WriteLine($"Data:{data}");
+            Console.ReadKey();
+        }
+    }
+}
